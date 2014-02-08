4cf18895ac59f93feded50706e0725a250f58e92
Jared Tyler <jared@jaredtyler.ca>
2014-02-08 18:52:34 -0500

    Now performing a free-form draw when clicking and dragging on the canvas.  No longer drawing dots.

--- public/js/connect.js
+++ public/js/connect.js
@@ -4,13 +4,29 @@ $(function() {
 		canvas = $('canvas'),
 		context = canvas.get(0).getContext('2d');
 	
+	// TODO: Not using 'radius' because this is a line, not a circle.
 	var _draw = function(data)
 	{
+		var stops = data.stops;
+		if (stops.length == 0) {
+			return;
+		}
+		
 		context.beginPath();
+		context.moveTo(stops[0].x, stops[0].y);
+		
+		for (var i = 1; i < stops.length; i++) {
+			context.lineTo(stops[i].x, stops[i].y);
+		}
+		
+		context.strokeStyle = data.colour;
+		context.stroke();
+		
+		/*context.beginPath();
 		context.arc(data.x, data.y, data.r, 0, Math.PI * 2, false);
 		context.closePath();
 		context.fillStyle = data.c;
-		context.fill();
+		context.fill();*/
 	};
 	
 	canvas.css('border', 'dotted 1px black')
@@ -19,7 +35,7 @@ $(function() {
 	
 	socket = io.connect('http://10.255.31.131');
 	
-	canvas.on('click', function(e) {
+	/*canvas.on('click', function(e) {
 		var data = {
 			x: e.offsetX,
 			y: e.offsetY,
@@ -29,6 +45,47 @@ $(function() {
 	
 		_draw(data);
 		socket.emit('draw', data);
+	});*/
+	
+	var isDrawing = false,
+		mouseStops = [];
+	canvas.on('mousedown', function(e) {
+		isDrawing = true;
+		mouseStops.length = 0;
+		mouseStops.push({
+			x: e.offsetX,
+			y: e.offsetY
+		});
+	});
+	canvas.on('mousemove', function(e) {
+		if (!isDrawing) {
+			return;
+		}
+		
+		mouseStops.push({
+			x: e.offsetX,
+			y: e.offsetY
+		});
+	});
+	canvas.on('mouseup', function(e) {
+		if (!isDrawing) {
+			return;
+		}
+		mouseStops.push({
+			x: e.offsetX,
+			y: e.offsetY
+		});
+		
+		var data = {
+			stops: mouseStops,
+			radius: 25,
+			colour: '#000'
+		};
+		
+		_draw(data);
+		socket.emit('draw', data);
+		
+		isDrawing = false;
 	});
 	
 	socket.on('draw', function(data) {
