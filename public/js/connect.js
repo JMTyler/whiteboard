1bf609a79f0a15a0d19741113dd0417852ab0525
Jared Tyler <jared@jaredtyler.ca>
2014-02-08 18:50:06 -0500

    Including jQuery and switching all relevant code to use it.

--- public/js/connect.js
+++ public/js/connect.js
@@ -1,37 +1,38 @@
 
-function onload()
-{
-	var canvas = document.getElementById('myCanvas');
-		canvas.style.border = 'dotted 1px black';
-		canvas.width = 500;
-		canvas.height = 500;
+$(function() {
+	var socket,
+		canvas = $('canvas'),
+		context = canvas.get(0).getContext('2d');
 	
-	var context = canvas.getContext('2d');
-	var socket = io.connect('http://10.255.31.131');
-	
-	function draw(data)
+	var _draw = function(data)
 	{
 		context.beginPath();
 		context.arc(data.x, data.y, data.r, 0, Math.PI * 2, false);
 		context.closePath();
 		context.fillStyle = data.c;
 		context.fill();
-	}
+	};
 	
-	canvas.addEventListener('click', function(e) {
+	canvas.css('border', 'dotted 1px black')
+		.prop('width', 500)
+		.prop('height', 500);
+	
+	socket = io.connect('http://10.255.31.131');
+	
+	canvas.on('click', function(e) {
 		var data = {
-			x: e.pageX - canvas.offsetLeft,
-			y: e.pageY - canvas.offsetTop,
+			x: e.offsetX,
+			y: e.offsetY,
 			r: 25,
 			c: '#000'
 		};
 	
-		draw(data);
+		_draw(data);
 		socket.emit('draw', data);
 	});
 	
 	socket.on('draw', function(data) {
 		console.log('drawing from other user', data);
-		draw(data);
+		_draw(data);
 	});
-}
+});
