9faec95b7bcd4b0e37a19ebb2788e438bbd99fce
Jared Tyler <jared@jaredtyler.ca>
2014-02-08 17:01:56 -0500

    Playing around with socket.io

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-//Globals
-GLOBAL.fs      = require('fs');
-GLOBAL._       = require('underscore');
-GLOBAL.express = express = require("express");
-GLOBAL.app     = express();
-GLOBAL.ejs     = require('ejs');
-
-//Express
-app.engine('html', ejs.renderFile);
-app.use(express.bodyParser());
-app.use(express.cookieParser());
-app.use(express.static(__dirname + '/public'));
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-	console.log("Listening on " + port);
-});
+// Globals
+GLOBAL.fs       = require('fs');
+GLOBAL._        = require('underscore');
+GLOBAL.express  = require("express");
+GLOBAL.app      = express();
+GLOBAL.ejs      = require('ejs');
+GLOBAL.server   = require('http').createServer(app);
+GLOBAL.io       = require('socket.io').listen(server);
 
 //Modules
 mw  = require('./modules/middleware.js');
 xo  = require('./modules/xo-node.js');
 
-//Renders HTML file with layout.ejs
+// Express
+app.engine('html', ejs.renderFile);
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.static(__dirname + '/public'));
+
+
+// Web server
+var port = process.env.PORT || 80;
+server.listen(port, function() {
+	console.log("Listening on " + port);
+});
+
+// Renders HTML file with layout.ejs
 var render = function(htmlFile, vars){
 	var tempRender = ejs.render(fs.readFileSync(__dirname + '/views/' + 'layout.ejs','utf8'), {
 		body : fs.readFileSync(__dirname + '/views/' + htmlFile,'utf8'),
@@ -28,22 +33,29 @@ var render = function(htmlFile, vars){
 };
 
 
-//Routes
+// Routes
 app.get('/', function(req,res){
-	res.end(render('index.html', {
-		things : 'test'
-	}));
+	res.end(render('index.html'));
 });
 
-app.get('/thing/:thingid', function(req,res){
+/*app.get('/thing/:thingid', function(req,res){
 	Things.findById(req.params.thingid, function(err, thing){
 		if(err || !thing) return res.send(500, "Can't find thing");
 		res.end(render('thingPage.html', {
 			thing : JSON.stringify(thing)
 		}));
 	});
-});
+});*/
 
 app.get('*', function(req,res){
 	res.end(render('oops.html'));
+});
+
+
+//Socket server
+io.sockets.on('connection', function(socket) {
+	socket.emit('news', {hello: 'world'});
+	socket.on('my other event', function(data) {
+		console.log('LOOK AT ME MAH', data);
+	});
 });
